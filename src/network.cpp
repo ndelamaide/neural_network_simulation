4058cf65f86a2a477ef5c2ad1d6ba807e6b7d70d
ndelamaide <nicolas.delamaide@gmail.com>
2018-10-26 20:10:19 +0200

    First working version

--- src/network.cpp
+++ src/network.cpp
@@ -1,32 +1,61 @@
 #include "network.h"
 #include "random.h"
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-void network::resize(const size_t& n){
+void Network::resize(const size_t& n){
   values.clear();
   values.resize(n);
   RNG.normal(values, 0, 1);
 }
 
-bool network::add_link(const size_t& first, const size_t& second){
-  if(((first<values.size()) and (second<values.size()) and (first!=second))
-    and ((make_pair(first,second)!=links.find(first)) and (make_pair(second,first)!=links.find(second)))){
-    links.insert(make_pair(first,second),make_pair(second,first));
+//Mauvaise conditions
+bool Network::add_link(const size_t& first, const size_t& second){
+  bool exists(false);
+  vector<size_t> connections(neighbors(first));
+  for(auto connection: connections){
+    if(connection==second){
+      exists=true;
+    }
+  }
+  if((first<values.size()) and (second<values.size()) and (first!=second)
+  and (!exists)){
+    //  auto order1(links.find(first));
+      //auto order2(links.find(second));
+      //links.insert(order1,make_pair(first,second));
+      //links.insert(order2,make_pair(second,first));
+      links.insert(make_pair(first,second));
+      links.insert(make_pair(second,first));
     return true;
   } else { return false; }
 }
 
-size_t network::random_connect(const double& mean){
-  for(size_t i(0); i< values.size(); ++i){
-    while(add_link(i, RNG.uniform_double(0,values.size())) or (int j(0)<=RNG.poisson(mean))){
-      ++j;
-    }
+//not right
+size_t Network::random_connect(const double& mean){
+  links.clear();
+  size_t connect(0);
+  vector<size_t> random_nodes(values.size());
+  for(size_t i(0); i < random_nodes.size();++i){
+    random_nodes[i]=i;
   }
+  RNG.shuffle(random_nodes);
+  int size(random_nodes.size());
+  for(int i(0); i< size;++i){
+    int poisson(RNG.poisson(mean));
+    for(int j(1);j <= poisson;++j){
+      if(add_link(random_nodes[random_nodes.size()-1],j)){
+        ++connect;
+      }
+    }
+    random_nodes.pop_back();
+    RNG.shuffle(random_nodes);
+  }
+  return connect;
 }
 
-size_t network::set_values(const std::vector<double>& newV){
+size_t Network::set_values(const std::vector<double>& newV){
   size_t success(0);
   for(int i(0); (i< newV.size()) and (i<values.size()); ++i){
     values[i]=newV[i];
@@ -35,29 +64,30 @@ size_t network::set_values(const std::vector<double>& newV){
   return success;
 }
 
-size_t network::size() const{
+size_t Network::size() const{
   return values.size();
 }
 
-size_t network::degree(const size_t &_n) const{
+size_t Network::degree(const size_t &_n) const{
   return links.count(_n);
 }
 
-double network::value(const size_t &_n) const{
+double Network::value(const size_t &_n) const{
   return values[_n];
 }
 
-vector<double> network::sorted_values() const{
+vector<double> Network::sorted_values() const{
   vector<double> sorted(values);
-  sort(sorted.begin(),sorted.end());
+  sort(sorted.rbegin(),sorted.rend());
   return sorted;
 }
 
-vector<size_t> network::neighbors(const size_t& k) const{
+vector<size_t> Network::neighbors(const size_t& k) const{
   auto range(links.equal_range(k));
-  vector<size_t> neighbors();
+  vector<size_t> neighbor;
   for(auto i(range.first); i!=range.second; ++i){
-    neighbors.push_back(i->second);
+    neighbor.push_back(i->second);
   }
-  return neighbors;
+  sort(neighbor.rbegin(),neighbor.rend());
+  return neighbor;
 }
